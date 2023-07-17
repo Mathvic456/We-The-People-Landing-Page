4cdee053ebe7319faae140ef4bae8388ec3e0215
Anthony <anthonychinaemeru84@yahoo.com>
2023-07-17 23:22:24 +0100

    Added flex-box to the hero items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,14 +7,23 @@ export const Header = () => {
     <div className='heroSection'>
         <Navbar />
         <Socials />
-        <div className="heroSideBox p-3">
-          <div className="title">
-            <div className="play">
-              <p><CiPlay1 /></p>
+        <div className="wrapper">
+          <div className="heroSideBox p-3">
+            <div className="title">
+              <div className="play">
+                <p><CiPlay1 /></p>
+              </div>
+              <h3 className="text-xl text-white font-semibold"> A Story to Tell</h3>
             </div>
-            <h3 className="text-xl text-white font-semibold"> A Story to Tell</h3>
+              <p className="text-sm text-white font-thin p-3"> From Nollywood to politics. Watch Funke Akindele`s journey into runneing for the office of the Deputy Govrnor</p>
           </div>
-            <p className="text-sm text-white font-thin p-3"> From Nollywood to politics. Watch Funke Akindele`s journey into runneing for the office of the Deputy Govrnor</p>
+
+          <div className="heroRightText">
+            <h2>
+              For the People <br /> and By <span>The People</span>
+            </h2>
+            <p>Know your leaders, stay informed on current updates, <br /> create and share polls, and join online town halls to <br /> stay updated about the Nigerian political spaceå</p>
+          </div>
         </div>
     </div>
   )
